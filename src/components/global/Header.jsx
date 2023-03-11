6c72e712a140917a978ba11ee8518e5033f94b40
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-11 20:02:16 +0530

    Header Navigation For Home

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
 	return (
 		<header className="bg-white shadow">
 			<div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-				<h1 className="text-2xl font-bold text-gray-900">
-					Flavor Frenzy
-				</h1>
+				<Link href="/">
+					<h1 className="text-2xl font-bold text-gray-900 cursor-pointer">
+						Flavor Frenzy
+					</h1>
+				</Link>
 				<nav className="space-x-4">
 					<a
 						href="#"
